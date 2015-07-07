ff7760e71142a31cd31261a0d7aa3ceaef3c40a9
Helen Sherwood-Taylor <helenst@gmail.com>
2015-07-07 18:53:27 +0100

    Use new licence schema

--- licence_transformer.py
+++ licence_transformer.py
@@ -15,14 +15,30 @@ while True:
 
     # Can't have an empty country jurisdiction due to schema restrictions
     # so have to leave out the whole row.
-    if raw_record['country']:
-        licence_record = {
-            "source_url": raw_record['source_url'],
-            "company_name": raw_record['name'],
-            "company_jurisdiction": raw_record['country'],
-            "sample_date": raw_record["sample_date"],
-            "licence_jurisdiction": "Oman",
-            "category": "Financial",
-        }
+    licence_record = {
+        "source_url": raw_record['source_url'],
+        "sample_date": raw_record["sample_date"],
+        "category": ["Financial"],
+        "licence_holder": {
+            "entity_type": "company",
+            "entity_properties": {
+                "name": raw_record['name'],
+                "mailing_address": raw_record['box'],
+                "jurisdiction": raw_record['country'],
+            }
+        },
+        "permissions": [],
+        "licence_issuer": {
+            "name": "Bank of Oman",
+            "jurisdiction": "Oman",
+        },
+        "jurisdiction_of_licence": "Oman",
+    }
 
-        print json.dumps(licence_record)
+    if raw_record['tel']:
+        licence_record['licence_holder']['entity_properties']['telephone_number'] = raw_record['tel']
+
+    if raw_record['fax']:
+        licence_record['licence_holder']['entity_properties']['fax_number'] = raw_record['fax']
+
+    print json.dumps(licence_record)
